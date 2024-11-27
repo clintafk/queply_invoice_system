793439d761314057c77bc713e2a13899a9a4f7a2
Timothy Robert Mutia <20210023599@my.xu.edu.ph>
2024-11-27 13:18:11 +0800

    forgot to export clientSchema

--- src/tables/clientSchema.tsx
+++ src/tables/clientSchema.tsx
@@ -10,7 +10,7 @@ import { MoreHorizontal, ArrowUpDown } from "lucide-react";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
-const clientSchema = z.object({
+export const clientSchema = z.object({
   id: z.number().int(),
   full_name: z.string({
     required_error: "This is required ok",
